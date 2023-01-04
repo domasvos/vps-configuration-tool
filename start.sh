b2f58751187857765cbc1c5052291f197a6ce53c
root <root@vps.server.local>
2023-01-04 20:33:01 +0000

    Adding choices feature

--- start.sh
+++ start.sh
@@ -26,3 +26,37 @@ grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf "CPU us
 
 # Set the text color back to the default
 echo -e "\033[0m"
+
+# Display the options menu
+echo "Please choose an option:"
+echo "1. Install CyberPanel"
+echo "2. Exit"
+
+# Read in the user's selection
+read -p "Enter your choice: " choice
+
+# Set the text color back to the default
+echo -e "\033[0m"
+
+# Handle the user's selection
+case $choice in
+    1)
+        # Display the necessary requirements for CyberPanel
+        echo -e "\033[31mPython is necessary and will be installed."
+        echo -e "CyberPanel requires at least 1024MB of Ram memory."
+        echo -e "CyberPanel requires at least 10GB of free disk space.\033[0m"
+
+        # Ask the user if they still want to install CyberPanel
+        read -p "Do you want to install CyberPanel (y/N)? " install_cyberpanel
+        if [[ $install_cyberpanel =~ ^[Yy]$ ]]; then
+            bash install_cyberpanel.sh
+        else
+            # Return to the options menu
+            bash start.sh
+        fi
+        ;;
+    2)
+        # Exit the script
+        exit 1
+        ;;
+esac
