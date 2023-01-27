cf8996aae0f819ff09ce7a4dc53396d139f9f9e4
domas.vosylius <domas.vosylius@sai360.com>
2023-01-27 15:10:21 +0200

    fixing

--- cms/wordpress.sh
+++ cms/wordpress.sh
@@ -1,52 +1,71 @@
 #!/bin/bash
 
-#Check what webserver is running
-if [ -x "$(command -v nginx)" ]; then
-    WEBSERVER='nginx'
-elif [ -x "$(command -v apache2)" ]; then
-    WEBSERVER='apache2'
-else
-    echo "Neither Apache nor Nginx is installed. Please install one of them and try again."
-    source ../start.sh
-fi
+# Function to check if a command is installed
+check_installed() {
+    if ! [ -x "$(command -v "$1")" ]; then
+        echo "$1 is not installed, installing it now"
+        sudo apt-get install -y "$1"
+    else
+        echo "$1 is already installed"
+    fi
+}
 
-#Adding loop for ability to install several WordPress websites
-i=1
-while [ -d "/var/www/html/wordpress$i" ]; do
-    i=$((i+1))
-done
+# Function to check which web server is running
+check_webserver() {
+    if [ -x "$(command -v nginx)" ]; then
+        WEBSERVER='nginx'
+    elif [ -x "$(command -v apache2)" ]; then
+        WEBSERVER='apache2'
+    else
+        echo "Neither Apache nor Nginx is installed. Please install one of them and try again."
+        exit 1
+    fi
+}
 
-# Install WordPress
-sudo apt-get update
-sudo mkdir -p /srv/www
-sudo chown www-data: /srv/www
-curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www/wordpress$i
+# Function to install WordPress
+install_wordpress() {
+    i=1
+    while [ -d "/var/www/html/wordpress$i" ]; do
+        i=$((i+1))
+    done
+    sudo apt-get update
+    sudo mkdir -p /srv/www/wordpress$1
+    sudo chown www-data: /srv/www/wordpress$1
+    curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www/wordpress$i
+    sudo ln -s /srv/www/wordpress$i /var/www/html/wordpress$i
+    sudo chown -R www-data:www-data /var/www/html/wordpress$i
+}
 
-#Checking and installing dependancy
-deps=("ghostscript" "libapache2-mod-php" "mysql-server" "php" "php-bcmath" "php-curl" "php-imagick" "php-intl" "php-json" "php-mbstring" "php-mysql" "php-xml" "php-zip")
+# Function to configure the database for WordPress
+configure_database() {
+    if [ -x "$(command -v mysql)" ]; then
+        echo "MySQL is already installed"
+    else
+        check_installed "mysql-server"
+    fi
+    DBNAME="wordpress$i"
+    sudo mysql -e "CREATE DATABASE $DBNAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+    sudo mysql -e "GRANT ALL ON $DBNAME.* TO 'wpuser'@'localhost' IDENTIFIED BY 'password';"
+    sudo mysql -e "FLUSH PRIVILEGES;"
+}
+
+# Function to configure WordPress for the chosen web server
+configure_webserver() {
+    if [ "$WEBSERVER" == "nginx" ]; then
+        sudo service nginx restart
+    else
+        sudo service apache2 restart
+    fi
+}
+
+# Check and install dependencies
+deps=("ghostscript" "libapache2-mod-php" "php" "php-bcmath" "php-curl" "php-imagick" "php-intl" "php-json" "php-mbstring" "php-mysql" "php-xml" "php-zip")
 for dep in "${deps[@]}"
 do
-    if ! [ -x "$(command -v "$dep")" ]; then
-        echo "$dep is not installed, installing it now"
-        sudo apt-get install -y "$dep"
-    else
-        echo "$dep is already installed"
-    fi
+    check_installed "$dep"
 done
 
-# Configure WordPress for the chosen web server
-if [ "$WEBSERVER" == "nginx" ]; then
-    sudo ln -s /srv/www/wordpress$i /var/www/html/wordpress$i
-    sudo chown -R www-data:www-data /var/www/html/wordpress$i
-    sudo service nginx restart
-else
-    sudo ln -s /srv/www/wordpress$i /var/www/html/wordpress$i
-    sudo chown -R www-data:www-data /var/www/html/wordpress$i
-    sudo service apache2 restart
-fi
-
-# Configure the database for WordPress
-DBNAME="wordpress$i"
-sudo mysql -e "CREATE DATABASE $DBNAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
-sudo mysql -e "GRANT ALL ON $DBNAME.* TO 'wpuser'@'localhost' IDENTIFIED BY 'password';"
-sudo mysql -e "FLUSH PRIVILEGES;"
+check_webserver
+install_wordpress
+configure_database
+configure_webserver
