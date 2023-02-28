d1e0962db7382cc7f8fce56445444668ea053079
Domas Vosylius <domas.vosylius@visma.com>
2023-02-28 16:34:19 +0200

    adding php-fpm

--- cms/opencart/opencart_debian.sh
+++ cms/opencart/opencart_debian.sh
@@ -7,7 +7,7 @@ prerequisites() {
 
 check_modules() {
     # Dependancies
-    deps=("libapache2-mod-php8.1" "php8.1" "php8.1-bcmath" "php8.1-curl" "php8.1-imagick" "php8.1-intl" "php8.1-json" "php8.1-mbstring" "php8.1-mysql" "php8.1-xml" "php8.1-zip" "php8.1-gd")
+    deps=("libapache2-mod-php8.1" "php8.1" "php8.1-bcmath" "php8.1-curl" "php8.1-imagick" "php8.1-intl" "php8.1-json" "php8.1-mbstring" "php8.1-mysql" "php8.1-xml" "php8.1-zip" "php8.1-gd" "php8.1-fpm")
 
     for dep in "${deps[@]}"
     do
